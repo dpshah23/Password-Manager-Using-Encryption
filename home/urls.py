99ecd91ebc6bd524e2046f0c0f217160e041f86e
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-05-24 12:33:03 +0530

    Added Signup

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('login', views.login),
     path('', views.home),
-    # path('/signup', views.signup),
+    path('signup', views.signup),
 ]
--- home/views.py
+++ home/views.py
@@ -13,12 +13,12 @@ def login(request):
         print(email,password)
         
         try:
-            # authemail=os.getenv('email')
-            # authpassword=os.getenv('password')
+            authemail=os.getenv('email')
+            authpassword=os.getenv('password')
             # print(authemail,authpassword)
-            # user=auth.sign_in_with_email_and_password(authemail,authpassword)
+            user=auth.sign_in_with_email_and_password(authemail,authpassword)
             authuser=auth.sign_in_with_email_and_password(email,password)
-            getname=db.child('users').child(email).get(authuser['idToken']).val()
+            getname=db.child('users').child(email).get(user['idToken']).val()
             name=getname['name']
             request.session['name']=name
             request.session['email']=email
@@ -39,5 +39,35 @@ def home(request):
         
         return render(request,'home.html')
 
+def signup(request):
 
+    if request.method=="POST":
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        name=request.POST.get('name')
+        mobile=request.POST.get('mobile')
+        print(email,password,name)
+        authemail=os.getenv('email')
+        authpassword=os.getenv('password')
+        print(authemail,authpassword)
+        try:
+            auth.create_user_with_email_and_password(email,password)
+            user=auth.sign_in_with_email_and_password(authemail,authpassword)
+            print(user)
+            
+            data={
+                'name':name,
+                'email':email,
+                'mobile':mobile,
+                'password':password
+
+            }
+            db.child('users').child(email).set(data,user['idToken'])
+            messages.success(request,f"Account Created Successfully")
+            return redirect("/login")
+        except Exception as e:
+            print(e)
+            messages.error(request,"Email Already Exist")
+            return render(request,'signup.html')
+    return render(request,'signup.html')
 # Create your views here.
