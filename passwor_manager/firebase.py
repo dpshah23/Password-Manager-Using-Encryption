a1a6c8c792cb9b538d5b2e11ed1267a54d832860
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-05-24 10:16:44 +0530

    Added firebase file

---
+++ passwor_manager/firebase.py
@@ -0,0 +1,8 @@
+
+import pyrebase
+from django.conf import settings
+
+firebase = pyrebase.initialize_app(settings.FIREBASE_CONFIG)
+auth = firebase.auth()
+db = firebase.database()
+storage = firebase.storage()
